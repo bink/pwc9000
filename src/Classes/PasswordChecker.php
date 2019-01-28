2910782af5b517974ef243f34b21315c9390d83c
Bink <bink@users.noreply.github.com>
2019-01-28 10:30:51 +0100

    Added more secure password checking

--- src/Classes/PasswordChecker.php
+++ src/Classes/PasswordChecker.php
@@ -1,6 +1,22 @@
 <?php
 class PasswordChecker
 {
+	public $pw;
+
+	public function __construct($pw)
+	{
+		$this->pw = $pw;
+	}
+
+	public function RealCheckPassword($password)
+	{
+		if ($password == $this->pw)
+		{
+			return true;
+		}
+		return false;
+	}
+
 	public function CheckPassword($password)
 	{
 		if($password == "t]c#5\{T)w@;XKtA")
