948d8f1ca4cb307ace3a8ed0ca37e4a2537086b3
Bink <bink@users.noreply.github.com>
2019-01-28 10:29:23 +0100

    Added Password Check function

--- src/Classes/PasswordChecker.php
+++ src/Classes/PasswordChecker.php
@@ -1,3 +1,13 @@
 <?php
-	// TODO Implement
+class PasswordChecker
+{
+	public function CheckPassword($password)
+	{
+		if($password == "t]c#5\{T)w@;XKtA")
+		{
+			return true;
+		}
+		return false;
+	}
+}
 ?>
